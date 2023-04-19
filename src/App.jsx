5736981583d4248e4ab1b636b20723b92071ac36
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2023-04-19 21:40:00 +0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,24 @@ import CategoryProducts from './components/CategoryProducts'
 import Profilepage from './components/Profilepage'
 import LoginPage from './components/LoginPage'
 import RegisterPage from './components/RegisterPage'
-import { domain,header,usertoken } from './env'
+import { domain, header, usertoken } from './env'
 import { useGlobalState } from './state/provider'
+import Cart from './components/Cart'
 
 const App = () => {
   //console.log(usertoken,"User token");
-  const [{ profile },dispatch] = useGlobalState()
-  //console.log(profile,"$$4user profile");
-  useEffect(()=>{
-    if(usertoken!==null){
+  const [{ profile, reloadpage, cartproduct_complit, cartproductf_uncomplit }, dispatch] = useGlobalState()
+  //console.log(cartproduct_complit,"$complit cart");
+  // console.log(cartproductf_uncomplit,"$uncomplit cart");
+
+  useEffect(() => {
+    if (usertoken !== null) {
       const getdata = async () => {
         await Axios({
           method: "get",
           url: `${domain}/api/profile/`,
           headers: header
-        }).then(response=>{
+        }).then(response => {
           //console.log(response.data["data"],"$$444 user profile")
           dispatch({
             type: "ADD_PROFILE",
@@ -32,19 +35,65 @@ const App = () => {
       }
       getdata()
     }
+  }, [reloadpage])
+
+  useEffect(() => {
+    const getdata = async () => {
+      await Axios({
+        method: "get",
+        url: `${domain}/api/cart/`,
+        headers: header
+      }).then(res => {
+        //console.log(res.data, "##3 CArts my");
+        {
+          const all_data = []
+          res?.data.map(data => {
+            if (data.complit) {
+              all_data.push(data)
+              dispatch({
+                type: "ADD_CARTPRODUCT_COMPLIT",
+                cartproduct_complit: all_data
+              })
+              // console.log(true);
+            }
+            else {
+              dispatch({
+                type: "ADD_CARTPRODUCT_UNCOMPLIT",
+                cartproductf_uncomplit: data
+              })
+              // console.log(false)
+            }
+          })
+        }
+      })
+    }
+    getdata()
   }, [])
+
+
   return (
     <BrowserRouter>
-    <Navbar/>
+      <Navbar />
       <Switch>
-        <Route exact path="/" component={HomePage}/>
-        <Route exact path="/product/:id" component={ProductDetails}/>
-        <Route exact path="/category/:id" component={CategoryProducts}/>
-        <Route exact path='/profile' component={Profilepage} />
-        <Route exact path='/login' component={LoginPage} />
-        <Route exact path='/register' component={RegisterPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/product/:id" component={ProductDetails} />
+        <Route exact path="/category/:id" component={CategoryProducts} />
+        {
+          profile !== null ? (
+            <>
+              <Route exact path='/profile' component={Profilepage} />
+              <Route exact path='/cart' component={Cart} />
+            </>
+          ) :
+            (
+              <>
+                <Route exact path='/login' component={LoginPage} />
+                <Route exact path='/register' component={RegisterPage} />
+              </>
+            )
+        }
 
-        <Route exact component={HomePage}/>
+        <Route exact component={HomePage} />
       </Switch>
     </BrowserRouter>
   )
