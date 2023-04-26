a89da79c682fcbfd021525cf4b5aa905ba4782d7
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2023-04-26 19:42:16 +0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ const App = () => {
       })
     }
     getdata()
-  }, [])
+  }, [reloadpage])
 
 
   return (
--- src/env.js
+++ src/env.js
@@ -3,9 +3,14 @@ import Cookies from 'js-cookie';
 
 
  export const domain = "http://127.0.0.1:8000";
+ //export const domain = "";
  export const usertoken = window.localStorage.getItem("token")
  export const header = {
     Authorization: `token ${usertoken}`
+}
+const csrftoken = Cookies.get('csrftoken')
+export const header2 = {
+    'X-CSRFToken': csrftoken,
 }
 //export const domain = "";
 
